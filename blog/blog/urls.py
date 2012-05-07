dcbe57905e108f5aa2f5b23d1c524e787217696c
Cory Benfield <lukasaoz@gmail.com>
2012-05-07 21:54:37 +0100

    Set up a default home page.
    While working on development, we can take advantage of the hero.html
    page from Twitter Bootstrap as a default page.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'blog.views.home', name='home'),
+    url(r'^$', 'posts.views.home', name='home'),
     # url(r'^blog/', include('blog.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
