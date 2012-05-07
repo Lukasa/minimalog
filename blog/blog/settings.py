88535fe7f1403c7f4925b96829c2440df5e4c4b0
Cory Benfield <lukasaoz@gmail.com>
2012-05-07 22:09:50 +0100

    Add storages for use with S3.

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'posts',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
