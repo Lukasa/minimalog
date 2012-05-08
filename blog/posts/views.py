fff5e7830c2e6ccb4446185b69d410b026d7da52
Cory Benfield <lukasaoz@gmail.com>
2012-05-08 15:47:58 +0100

    Set up the main page.
    This page currently lacks logic for showing the most recent posts.
    That's fine, it's just a placeholder. Additionally, much of the CSS will
    be changed at some point in the future.

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-    return render_to_response('hero.html', context_instance=RequestContext(request))
+    return render_to_response('home.html', context_instance=RequestContext(request))
