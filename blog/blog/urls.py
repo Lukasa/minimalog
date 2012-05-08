e7482c1a5cfd53e3054baaca4578c0d6f85f1124
Cory Benfield <lukasaoz@gmail.com>
2012-05-08 19:55:40 +0100

    Add the framework for individual blog posts.
    This is quite a sizable change that implements all of the required
    functionality for displaying blog posts, including the rendering from
    Markdown. Amazingly, it actually works.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'posts.views.home', name='home'),
+    url(r'^blog/(\w+)/$', 'posts.views.post', name='blogpost'),
     # url(r'^blog/', include('blog.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.template import RequestContext, loader
+from posts.models import Post
+from django.http import Http404, HttpResponse
 
 def home(request):
     return render_to_response('home.html', context_instance=RequestContext(request))
+
+def post(request, post_title):
+
+    # Try to find the post that corresponds to the title.
+    try:
+        found_post = Post.objects.get(title = post_title)
+    except Post.DoesNotExist:
+        raise Http404
+
+    title = u'Lukasa | ' + found_post.display_title
+
+    # Got to build up the relevant contexts.
+    context = RequestContext(request,
+            {'PAGE_TITLE': title,
+             'PAGE_DESCRIPTION': None,
+             'PAGE_AUTHOR': found_post.author.name,
+             'BODY': found_post.body})
+
+    t = loader.get_template('post.html')
+
+    return HttpResponse(t.render(context))
+
