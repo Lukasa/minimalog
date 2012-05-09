de7d7653ff5af1ad61923640c0c02f0135e76411
Cory Benfield <lukasaoz@gmail.com>
2012-05-09 21:10:09 +0100

    Add the ability to display comments.
    What would a blog be without comments? This change includes markup and
    CSS for displaying comments on blog posts. What this change does not
    include is the the ability to add comments.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
     def __unicode__(self):
         return self.title
 
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post)
+
+    def __unicode__(self):
+        return self.author + u"'s comment on " + self.post.__unicode__()
+
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext, loader
-from posts.models import Post
+from posts.models import Post, Comment
 from django.http import Http404, HttpResponse
 
 def post(request, post_title):
@@ -10,12 +10,16 @@ def post(request, post_title):
     except Post.DoesNotExist:
         raise Http404
 
+    # Get the information in the form we want. This should be considered
+    # subject to change.
     post_content = found_post.body.split('\n\n')
     post_title = post_content[0].strip('# ')
     post_remainder = u'\n\n'.join(post_content[1:])
     post_url = u'/blog/' + found_post.title + u'/'
-
     title = u'Lukasa | ' + post_title
+
+    # Get all the comments associated with the post.
+    comments = Comment.objects.filter(post = found_post).order_by("date")
 
     # Got to build up the relevant contexts.
     context = RequestContext(request,
@@ -24,7 +28,8 @@ def post(request, post_title):
              'PAGE_AUTHOR': u'Cory Benfield',
              'post_title': post_title,
              'post_body': post_remainder,
-             'post_url': post_url})
+             'post_url': post_url,
+             'comments': comments})
 
     t = loader.get_template('post.html')
 
