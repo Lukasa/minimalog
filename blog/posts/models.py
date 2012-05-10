715dcb62966b5c80544ed9eee79a6c69d3b9d927
Cory Benfield <lukasaoz@gmail.com>
2012-05-10 21:27:08 +0100

    Remove display_title field from Post model.
    It wasn't being used anyway.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Post(models.Model):
     body = models.TextField()
     title = models.CharField(max_length=50)
-    display_title = models.CharField(max_length=50)
     publication_date = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
