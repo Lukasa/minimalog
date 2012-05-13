26bac2e3f01ed6b7b631f9dec8d2c3156e9ce01a
Cory Benfield <lukasaoz@gmail.com>
2012-05-13 14:53:15 +0100

    Remove '/blog/' from the post url.
    Was pretty unnecessary.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('posts.views',
     # Examples:
     url(r'^$', 'home', name='blog_home'),
-    url(r'^blog/(?P<post_year>\d{4})/(?P<post_month>\d{2})/(?P<post_title>\w+)/$',
+    url(r'^(?P<post_year>\d{4})/(?P<post_month>\d{2})/(?P<post_title>\w+)/$',
         'post',
         name='blog_post'),
     url(r'^archive/$', 'archive', name='blog_archive'),
