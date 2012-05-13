2652345945adf312ae216190c112c3d321fd9243
Cory Benfield <lukasaoz@gmail.com>
2012-05-13 13:30:35 +0100

    More tests.

--- blog/posts/tests.py
+++ blog/posts/tests.py
@@ -1,6 +1,7 @@
+# coding=utf-8
 from django.test import TestCase
 from posts.helpers import get_post_url, post_as_components
-from posts.models import Post
+from posts.models import Post, Comment
 
 class PostViewsTestCase(TestCase):
     fixtures = ['posts_test_data.json']
@@ -92,6 +93,72 @@ class PostViewsTestCase(TestCase):
         self.assertTrue(len(response.context['post_url']) > 0)
         self.assertTrue(response.context['comments_enabled'])
         self.assertEqual(len(response.context['comments']), 1)
+
+    def test_post_page_handles_disabled_comments(self):
+        response = self.client.get('/blog/2012/05/2_Building_A_Blog_Part_2/')
+        self.assertEqual(response.context['comments_enabled'], False)
+        self.assertEqual(len(response.context['comments']), 0)
+
+    def test_post_comment_handles_valid_data(self):
+        post = Post.objects.get(pk = 3)
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(len(comments), 1)
+
+        response = self.client.post('/blog/2012/05/3_Building_A_Blog_Part_3/',
+                                    {'name': 'Fred',
+                                     'text': 'This is a comment.'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'],
+                    'http://testserver/blog/2012/05/3_Building_A_Blog_Part_3/')
+
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(len(comments), 2)
+        self.assertEqual(comments[1].author, 'Fred')
+        self.assertEqual(comments[1].text, 'This is a comment.')
+
+    def test_post_comment_handles_invalid_data(self):
+        # 404 if we post to a nonexistent post.
+        response = self.client.post('/blog/2012/5/4_Not_A_Post/')
+        self.assertEqual(response.status_code, 404)
+
+        # Confirm that we have the right number of comments.
+        post = Post.objects.get(pk = 3)
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(len(comments), 1)
+
+        # Try not sending data.
+        response = self.client.post('/blog/2012/05/3_Building_A_Blog_Part_3/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['form'].errors,
+                         {'name': [u'This field is required.'],
+                          'text': [u'This field is required.']})
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(len(comments), 1)
+
+        # Try junk data.
+        response = self.client.post('/blog/2012/05/3_Building_A_Blog_Part_3/',
+                                    {'foo': 'bar', 'greasy': 'spoon'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['form'].errors,
+                         {'name': [u'This field is required.'],
+                          'text': [u'This field is required.']})
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(len(comments), 1)
+
+    def test_post_comment_handles_unicode_properly(self):
+        post = Post.objects.get(pk = 3)
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(len(comments), 1)
+        name = u'ᄏ℗♩ヶ菱'
+        text = name
+
+        response = self.client.post('/blog/2012/05/3_Building_A_Blog_Part_3/',
+                                    {'name': name, 'text': text})
+        self.assertEqual(response.status_code, 302)
+
+        comments = Comment.objects.filter(post = post)
+        self.assertEqual(comments[1].author, u'ᄏ℗♩ヶ菱')
+        self.assertEqual(comments[1].text, u'ᄏ℗♩ヶ菱')
 
 class HelpersTestCase(TestCase):
     fixtures = ['posts_test_data.json']
